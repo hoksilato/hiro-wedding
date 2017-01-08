28620ddabfe9e1d471c45ed05abdc1694b0f9351
Cang Ta <hoksilato176@gmail.com>
2017-01-08 23:42:24 +0700

    refactor

--- app/operations/home/index.rb
+++ app/operations/home/index.rb
@@ -1,11 +1,12 @@
 module Home
   class Index < Operation
     def process
+      result.new('Hello World!')
     end
 
     private
     def result
-      Struct.new(:featured_cars)
+      Struct.new(:message)
     end
   end
 end
